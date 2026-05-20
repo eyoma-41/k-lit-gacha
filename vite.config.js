3cca963c18e454eb9ab226791cf80a8e3b31a241
eyoma-41 <hakgome@gmail.com>
2026-05-20 11:15:12 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+    plugins: [react()],
 });
